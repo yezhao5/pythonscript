45b461bab4b3d135e51833e1908fa5f2521d201a
Ye Zhao <cs8af46@ieng6-202.ucsd.edu>
2017-10-06 04:24:05 -0700

    don't have mkdir and scp and tar

--- script.py
+++ script.py
@@ -24,6 +24,21 @@ print ("this many students:", len(students))
 #for traking which tutor
 track = 0
 
-path = ("/home/linux/ieng6/cs8af/"+"cs8af46/"+"psa0_grading")
-os.mkdir (path);
+#to make dir under tutors' accounts for grading
+#path = ("/home/linux/ieng6/cs8af/"+"cs8afzz/"+"psa0_grading")
+#os.mkdir (path);
+
+for x in students:
+    path = ("/home/linux/ieng6/cs8af/"+"cs8afzz/"+"test.java")
+    check = os.path.isfile(path)
+    if check:
+        #create account in ta account with name of the student's account
+        #os.system("scp cs8afzz@ieng9.ucsd.edu:/test.java cs8af46@ieng9.ucsd.edu:/psa0_grading")
+        os.system("scp cs8afzz@ieng9.ucsd.edu:/test.java .")
+        #tar
+        count = os.listdir("/home/linux/ieng6/cs8af/"+"cs8af46")
+        if (len(count)>7):
+            track = track +1
+
+ 
 
