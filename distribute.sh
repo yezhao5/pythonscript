7ff13a93092ee4cc7e382cad4baaa9d56f249e76
Ye Zhao <cs8af46@ieng6-201.ucsd.edu>
2017-10-09 21:25:05 -0700

    backup for distribute with triger

--- distribute.sh
+++ distribute.sh
@@ -1,53 +1,59 @@
 #!/bin/bash
 
-if [ ! $1 ]; then
-	echo "Error: Too Few Arguments"
-	echo "Usage: $0 PA_NAME"
-	exit 1
-fi
+clas='cs8af'
 
 pa="$1"
+turnins="$2"
+collector="$3"
+tutors=0
+basedir="/home/linux/ieng6/cs8af/"
 
-basedir=`pwd`
-msdir="${basedir}/${pa}milestone.turnin"
+array=()
 
-mkdir -p ${pa}.results
-cd ${pa}.turnin
-
-final=0
-ms=0
-total=0
-
-for student in `cat "${basedir}/students"`; do
-	tarball="${student}.${pa}.tar.gz"
-	if [ -e $tarball ]; then
-		mkdir ${student}
-		cd ${student}
-
-		tar -zxf ../${tarball}
-		cp "${basedir}/runTests.${pa}" ./runTests
-		ms_mail="${msdir}/${student}/MILESTONE.MAIL"
-		if [ -e $ms_mail ]; then
-			cp "${ms_mail}" .
-		fi
-
-		final=$((final+1))
-		total=$((total+1))
-		cd ..
-	fi
-
-	if [ ! -d ${student} ] && [ -d "${msdir}/${student}" ]; then
-		mkdir ${student}
-		cd ${student}
-
-		cp "${msdir}/${student}/MILESTONE.MAIL" .
-		echo "Did Not Have a final turnin" >> SUMMARY.RESULTS
-
-		ms=$((ms+1))
-		total=$((total+1))
-		chmod g+rw SUMMARY.RESULTS
-		cd ..
-	fi
+for tutor in `ls ../ | grep "${clas}[1-9]"`;
+do
+    	array+=(${tutor})
+    	tutors=$((tutors+1))
 done
 
-echo "Processed ${pa}. total: ${total}, of which ${final} final turnins and ${ms} only milestones"
+let "avg=turnins/tutors"
+let "remainder=turnins%tutors"
+
+let "triger=tutors/remainder - 1"
+const=${triger}
+
+echo triger is ${triger}
+
+track=0
+
+if [ ! -e ${basedir}${array[${track}]}/${pa}_grading ]; then
+	mkdir  ${basedir}${array[${track}]}/${pa}_grading
+fi
+
+count=0
+
+for student in `ls ${pa}.turnin`; do
+	cp -r ${pa}.turnin/${student} ${basedir}${array[${track}]}/${pa}_grading
+	count=$((count+1))
+	echo ${count}
+	cd ${basedir}${array[${track}]}/${pa}_grading
+	if [ `ls -1 | wc -l` -ge "$avg" ]; then
+		if [["triger" -eq ]]; then
+			
+		elif [[ "$triger" -ne 1 ]]; then
+			track=$((track + 1))
+			if [ ! -e ${basedir}${array[${track}]}/${pa}_grading ]; then
+				mkdir  ${basedir}${array[${track}]}/${pa}_grading
+			fi
+			echo ${array[${track}]}
+			triger=$((triger-1))
+		else 
+			triger=${const}
+		fi 
+	fi 
+	cd ${basedir}cs8af${collector}
+done
+
+echo avg psa is ${avg}
+echo remainder is ${remainder}
+echo total numebr of tutors is ${tutors}
