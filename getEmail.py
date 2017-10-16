0f0d9ef2335dc5b0283ae5d241d733fa71d81e11
Ye Zhao <cs8af46@ieng6-202.ucsd.edu>
2017-10-15 23:28:14 -0700

    emails

--- getEmail.py
+++ getEmail.py
@@ -8,35 +8,40 @@ import subprocess
 import csv
 import tempfile
 
+
 studList = []
-#student account from grading sheet
-studList = open('Roster - Sheet1.tsv', 'rU')
-for line in studList:
+#all students
+studf = open('Roster - Sheet1.tsv', 'rU')
+studNum = 0
+for line in studf:
+    if studNum == 0:
+        studNum = studNum + 1
+        continue;
+    studNum = studNum + 1
     cells = line.rstrip('\n').split('\t')
     #append (Lastname, Firstname),email
     studList.append((cells[2], cells[7]))
 stud = dict(studList)
-print len(roster)
+studf.close()
 
 #get Firstname Lastname from finger
 
-col = 473
-csvfile = open ('emailList.csv', 'w')
-csvfile.write("Account,Name,Email\n")
+col = 471
+csvfile = open ('emailList.tsv', 'w')
+csvfile.write("Account\tName\tEmail\n")
 
-grading =  open('CSE8A PSA Grading - PA1.csv')
+grading =  open('CSE8A PSA Grading - PA1.csv', encoding='utf-8')
 mycsv = csv.reader(grading)
-mycsv = list (mycsv)
-lineNumber = 2
-readCol = 0
-for i in range (0, col)
-    account = mycsv[lineNumber][i]
-    csvfile.write(account+',')
-    readCol = readCol+1
-    
-    first = 'a'
-    last = 'a'
+curline = 0
+accounts = []
+for j in mycsv:
+    if curline == 2 :
+        accounts = j[1:]
+        #print(accounts)
+    curline = curline + 1
 
+for i in range (0, col):
+    account = accounts[i]
     with tempfile.TemporaryFile() as tempf:
         sys.path.insert(0,'/home/linux/ieng6/cs8af/')
         proc = subprocess.Popen(['finger', account], stdout=tempf)
@@ -44,19 +49,32 @@ for i in range (0, col)
         tempf.seek(0)
         output = tempf.read()
         sys.path.insert(0,'/home/linux/ieng6/cs8af/cs8af46/')
-        print(output)
+        #print(output)
+        if len(output.split()) < 6 :
+            continue;
         first = output.split()[3]
-        last = output.split()[4]
+        mid = output.split()[4]
+        last = output.split()[5]
+        mid = str(mid)
+
         first = str(first)
         last = str(last)
-
+        
+        mid = mid [2:len(mid)-1]
         first = first[2:len(first)-1]
         last = last[2:len(last)-1]
+        #print (last)
+        name = last+", "+first+" "+ mid
 
+        if last == "Directory:":
+            
+            name = mid+", "+first
 
-    name = last+", "+first
-    print (name)
-    email = stud[name]
-
-    csvfile.write(name+','+email+'\n')
+        print (name)
+    if name in stud:
+        email = stud[name]
+        csvfile.write(account+"\t"+name+"\t"+email+"\n")
+    else:
+        print("here.\n")
 csvfile.close()
+grading.close()
